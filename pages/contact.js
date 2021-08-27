6a8f9926d6cb65a7c66c720d9a19e5873d4fea4b
pineapple45 <gomraanmol@gmail.com>
2021-08-28 00:24:09 +0530

    netify forms test

--- pages/contact.js
+++ pages/contact.js
@@ -96,20 +96,16 @@ export default function Contact({ pageContent = [] }) {
       <h4>Contact</h4>
       <br />
       <div>
-        <form name='contact' method='POST' data-netlify='true'>
-          <input type='hidden' name='form-name' value='contact' />
+        <form name='contact' netlify>
           <p>
-            <input type='text' name='firstname' id='firstname' />
-            <label htmlFor='yourname'>Your Name:</label> <br />
-            <input type='text' name='name' id='yourname' />
+            <label>
+              Name <input type='text' name='name' />
+            </label>
           </p>
           <p>
-            <label htmlFor='youremail'>Your Email:</label> <br />
-            <input type='email' name='email' id='youremail' />
-          </p>
-          <p>
-            <label htmlFor='yourmessage'>Message:</label> <br />
-            <textarea name='message' id='yourmessage'></textarea>
+            <label>
+              Email <input type='email' name='email' />
+            </label>
           </p>
           <p>
             <button type='submit'>Send</button>
